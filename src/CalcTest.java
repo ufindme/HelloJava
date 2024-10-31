3ce24e809497a819ef0f0f720f163dc848830e6d
ufindme <8946695+ufindme@users.noreply.github.com>
2024-10-31 19:58:14 +0800

    add 3 cases

--- src/CalcTest.java
+++ src/CalcTest.java
@@ -4,6 +4,8 @@ public class CalcTest {
         int b = 20;
         int c = 25;
         int d = 25;
+        int e = 10;
+        int f = 20;
         System.out.println("a + b = " + (a + b) );
         System.out.println("a - b = " + (a - b) );
         System.out.println("a * b = " + (a * b) );
@@ -16,5 +18,8 @@ public class CalcTest {
         // 查看  d++ 与 ++d 的不同
         System.out.println("d++   = " +  (d++) );
         System.out.println("++d   = " +  (++d) );
+        System.out.println("(a < b) ? ( ++f ) : ( ++e )   = " + ((a < b) ?(++f):(++e)));
+        System.out.println("f   = " +  (f) );
+        System.out.println("e   = " +  (e) );
     }
 }
---
+++ src/CaseWhen.java
@@ -0,0 +1,8 @@
+public class CaseWhen {
+    public static void main(String args[]){
+        Integer i = new Integer(100);
+        int j = 100;
+        System.out.println(i == j); //false
+        System.out.println(i.equals(j));
+    }
+}
